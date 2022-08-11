f60c95570ceecd7d154c630ebc50b0cdb8c735fd
konnotakafumi4 <longwenjinye@gmail.com>
2022-08-11 14:29:17 +0000

    コメント非同期通信化

--- app/controllers/public/coffee_comments_controller.rb
+++ app/controllers/public/coffee_comments_controller.rb
@@ -4,11 +4,12 @@ class Public::CoffeeCommentsController < ApplicationController
     @comment = @coffee.coffee_comments.new(coffee_comment_params)
     @comment.end_user_id = current_end_user.id
     if @comment.save
-      redirect_to request.referer
+       @comments = @coffee.coffee_comments
+      #redirect_to request.referer
     else
       @coffee_new = Coffee.new
       @comments = @coffee.coffee_comments
-      redirect_to new_post_path
+      #redirect_to request.referer
     end
   end
 
@@ -16,7 +17,9 @@ class Public::CoffeeCommentsController < ApplicationController
     #@coffee = Coffee.find(params[:coffee_id])
     @comment = CoffeeComment.find(params[:id])
     @comment.destroy
-    redirect_to request.referer
+    @comments = @comment.coffee.coffee_comments
+    #redirect_to request.referer
+    #coffee_comment: @coffee_comment, coffee: @coffee, comments: @comments) %>");
   end
 
   def coffee_comment_params
